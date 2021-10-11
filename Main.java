e4c171a577a32efa8f1e9b2cb682f54945d7f86b
ITO Eigo <itoh@ccg.ac.jp>
2021-10-11 13:17:58 +0900

    showArgsメソッドを追加

--- Main.java
+++ Main.java
@@ -5,4 +5,10 @@ public class Main {
         System.out.println("Hello Japan!");
         System.out.println("Hello Java!");
     }
+
+    public static void showArgs(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
 }
