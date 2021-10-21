4e7ac60f3ff24985d51d8f3102b33179e9bc80b6
ITO Eigo <itoh@ccg.ac.jp>
2021-10-21 15:07:28 +0900

    「Test」の表示を追加

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Japan!");
         System.out.println("Hello Java!");
+
+        System.out.println("Test");
     }
 }
